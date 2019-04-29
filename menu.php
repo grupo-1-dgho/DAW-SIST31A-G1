55dc7d153260c3e1d0d99cc3eda4e22a0fcbab38
Tatiana Portillo <50118823+Tatiana1410@users.noreply.github.com>
2019-04-29 22:14:51 +0200

    menu
    archivo php

---
+++ menu.php
@@ -0,0 +1,82 @@
+
+
+<ul class=nav>
+<li><a href=Ejercicio1.php>Ejercicio1</a></li>
+<li><a href=Ejercicio2.php>Ejercicio2</a></li>
+<li><a href=Ejercicio3.php>Ejercicio3</a></li>
+<li><a href=Ejercicio4.php>Ejercicio4</a></li>
+</ul><br><br><br>
+<style type="text/css">
+ul{
+list-style:none;
+width: 100%;
+padding: 15px;
+}
+
+.nav > li {
+float:left;
+}
+.nav li a {
+background-color:black;
+color:#fff;
+text-decoration:none;
+display:block;
+list-style: none;
+padding: 10px 8px;
+
+			}
+			
+.nav li a:hover {
+background-color:blue;
+}
+			
+body{
+	background-image: url(fondo.jpg);
+	background-attachment: fixed;
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center;
+}
+			
+.form{
+	background-color: rgba(0,0,0,0.8);
+	border-radius: 15px;
+	border:2px solid blue;
+	width:65%;
+	padding: 15px;
+	box-sizing: border-box;
+	color: white;
+	font-size: 25px;
+	float: left;
+margin: 0px 20px;
+}
+input{
+	border-radius: 20px;
+	width: 200px;
+	height: 30px;
+}
+.boton{
+	border-radius: 20px;
+	width: 150px;
+	height: 30px;
+}
+	input:hover{
+		background-color: #2B91C7;
+		border:2px solid #33FF90;
+	}	
+	.rd{
+		width: 100px;
+	}	
+	article{
+	background-color: rgba(0,0,0,0.8);
+	border-radius: 15px;
+	border:2px solid blue;
+	width: 45%;
+	padding: 15px;
+	box-sizing: border-box;
+	color: white;
+	font-size: 25px;
+	float: left;
+margin: 0px 20px;
+}
+</style>
